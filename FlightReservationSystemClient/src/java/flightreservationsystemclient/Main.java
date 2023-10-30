841ff7d171c72a1a608a14b8ff71d103b8d0e25e
Jia yin <e0969393@u.nus.edu>
2023-10-30 23:30:03 +0800

    test
    test commit, commenting on main

--- FlightReservationSystemClient/src/java/flightreservationsystemclient/Main.java
+++ FlightReservationSystemClient/src/java/flightreservationsystemclient/Main.java
@@ -24,7 +24,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        //test
+        //test again
         List<Airport> retrieveAllAirports = airportSessionBeanRemote.retrieveAllAirports();
         
         for(Airport airport: retrieveAllAirports)
