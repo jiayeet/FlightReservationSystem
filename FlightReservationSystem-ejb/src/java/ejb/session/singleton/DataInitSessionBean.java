d9e8ce93b76eb26b9cf635f5b12421786d4f7948
Jia yin <e0969393@u.nus.edu>
2023-11-06 00:26:38 +0800

    Revert "Create Use cases 1-4"
    This reverts commit 42dab46e7d668767c7c0f1db399d33726e12e2b3.

--- FlightReservationSystem-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
+++ FlightReservationSystem-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -4,14 +4,8 @@
  */
 package ejb.session.singleton;
 
-import ejb.session.stateless.AircraftTypeSessionBeanLocal;
 import ejb.session.stateless.AirportSessionBeanLocal;
-import ejb.session.stateless.EmployeeSessionBeanLocal;
-import ejb.session.stateless.PartnerSessionBeanLocal;
-import entity.AircraftType;
 import entity.Airport;
-import entity.Employee;
-import entity.Partner;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -19,7 +13,6 @@ import javax.ejb.LocalBean;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import util.enumeration.EmployeeUserRoleEnum;
 
 /**
  *
@@ -30,17 +23,8 @@ import util.enumeration.EmployeeUserRoleEnum;
 @Startup
 public class DataInitSessionBean {
 
-    @EJB(name = "AircraftTypeSessionBeanLocal")
-    private AircraftTypeSessionBeanLocal aircraftTypeSessionBeanLocal;
-    @EJB(name = "PartnerSessionBeanLocal")
-    private PartnerSessionBeanLocal partnerSessionBeanLocal;
-    @EJB(name = "EmployeeSessionBeanLocal")
-    private EmployeeSessionBeanLocal employeeSessionBeanLocal;
     @EJB(name = "AirportSessionBeanLocal")
     private AirportSessionBeanLocal airportSessionBeanLocal;
-    
-    
-    
 
     @PersistenceContext(unitName = "FlightReservationSystem-ejbPU")
     private EntityManager em;
@@ -55,21 +39,6 @@ public class DataInitSessionBean {
         {
             airportSessionBeanLocal.createNewAirport(new Airport("SIN", "Merlion Airport", "Singapore", "Singapore", "Singapore"));
             airportSessionBeanLocal.createNewAirport(new Airport("TPE", "Merlion Airport", "Taipei", "Taiwan", "Taiwan"));
-        }
-        
-        if(em.find(Employee.class, 1l) == null)
-        {
-            employeeSessionBeanLocal.createNewEmployee(new Employee("firstName", "lastName", "systemAdministrator", "password", EmployeeUserRoleEnum.SYSTEMADMINISTRATOR));
-        }
-        
-        if(em.find(Partner.class, 1l) == null)
-        {
-            partnerSessionBeanLocal.createNewPartner(new Partner("firstName", "lastName", "partner", "password"));
-        }
-        
-        if(em.find(AircraftType.class, 1l) == null)
-        {
-            aircraftTypeSessionBeanLocal.createNewAircraft(new AircraftType("Boeing 737", 100));
         }
     }
 }
